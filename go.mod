219387b2165bbf107ca59d89cdf3af32cfe21091
savic-a <anastasijas557@gmail.com>
2022-02-18 04:22:25 +0100

    Nije jos kraj

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
-module Projekat
+module nasp
 
 go 1.17
-
-require github.com/spaolacci/murmur3 v1.1.0
--- structures/lsm-tree.go
+++ structures/lsm-tree.go
@@ -3,31 +3,35 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
+	"os"
+	"strconv"
+	"strings"
 )
 
 type LSM struct {
 	maxLevel int
 	maxSize  int
-	helper   []int  // pomocna struktura - trenutno stanje stabla na osnovu popunjenosti
+	helper   []int // pomocna struktura - trenutno stanje stabla na osnovu popunjenosti
 }
 
 func CreateLsm(maxL, maxS int) *LSM {
 	return &LSM{
 		maxLevel: maxL,
-		maxSize: maxS,
-		helper: make([]int, maxL, maxL),
+		maxSize:  maxS,
+		helper:   make([]int, maxL, maxL),
 	}
 }
 
-func (lsm LSM) IsCompactionNeeded(level int) bool{
+func (lsm LSM) IsCompactionNeeded(level int) bool {
 	if level == 1 {
-		lsm.helper[1]++    // cim dodamo novu sstabelu prvi nivo se povecava za 1
+		lsm.helper[1]++ // cim dodamo novu sstabelu prvi nivo se povecava za 1
 	}
 	return lsm.helper[level] == lsm.maxSize
 }
 
-func (lsm LSM) DoCompaction(level int) {
-	if level >= lsm.maxLevel {     // nema izvrsavanja kompakcija na poslednjem nivou
+func (lsm LSM) DoCompaction(dir string, level int) {
+	if level >= lsm.maxLevel { // nema spajanja tabela nakon poslednjeg nivoa
 		return
 	}
 
@@ -35,24 +39,80 @@ func (lsm LSM) DoCompaction(level int) {
 		return
 	}
 
-	lsm.DoCompaction(level+1)
+	dataFiles, indexFiles, summaryFiles := ReadFiles(dir, level)
+
+	i := 0
+
+	for i >= lsm.maxSize { // while petlja
+		fDataFile := dataFiles[i]
+		fIndexFile := indexFiles[i]
+		fSummaryFile := summaryFiles[i]
+		sDataFile := dataFiles[i+1]
+		sIndexFile := indexFiles[i+1]
+		sSummaryFile := summaryFiles[i+1]
+		Merge(dir, fDataFile, fIndexFile, fSummaryFile, sDataFile, sIndexFile, sSummaryFile, level)
+		i = i + 2
+	}
+
+	lsm.DoCompaction(dir, level+1)
 }
 
-func ReadFiles(dir string) {
-	files, err := ioutil.ReadDir(dir)
+func Merge(dir, fDFile, fIFile, fSFile, sDFile, sIFile, sSFile string, level int) {
+	strLevel := strconv.Itoa(level + 1)
+
+	// kreiranje nove sstabele
+	newData, _ := os.Create(dir + fDFile + sDFile + "lev" + strLevel + "-Data.bin")
+	newIndex, _ := os.Create(dir + fIFile + sIFile + "lev" + strLevel + "-Index.bin")
+	newSummary, _ := os.Create(dir + fSFile + sSFile + "lev" + strLevel + "-Summary.bin")
+
+	fDataFile, err := os.Open(dir + fDFile) // otvoren prvi data fajl
+	if err != nil {
+		log.Println("Fajl ne moze da se otvori, naziv fajla = ", fDFile)
+	}
+	defer fDataFile.Close()
+
+	sDataFile, _ := os.Open(dir + sDFile) // otvoren drugi data fajl
+	defer sDataFile.Close()
+
+	// brisanje starih sstabela
+	os.Remove(dir + fDFile)
+	os.Remove(dir + fIFile)
+	os.Remove(dir + fSFile)
+	os.Remove(dir + sDFile)
+	os.Remove(dir + sIFile)
+	os.Remove(dir + sSFile)
+}
+
+func ReadFiles(dir string, level int) ([]string, []string, []string) {
+	substr := strconv.Itoa(level)
+
+	files, err := ioutil.ReadDir(dir) // lista svih fajlova iz direktorijuma
 	if err != nil {
 		fmt.Println("greska!")
-		return
+		return nil, nil, nil
 	}
-	fmt.Println(files)
-	return
 
+	var dataFiles []string
+	var indexFiles []string
+	var summaryFiles []string
+
+	for _, f := range files {
+		if strings.Contains(f.Name(), substr+"-Data") {
+			dataFiles = append(dataFiles, f.Name())
+		}
+		if strings.Contains(f.Name(), substr+"-Index") {
+			indexFiles = append(dataFiles, f.Name())
+		}
+		if strings.Contains(f.Name(), substr+"-Summary") {
+			summaryFiles = append(dataFiles, f.Name())
+		}
+	}
+
+	return dataFiles, indexFiles, summaryFiles
 }
-
 
 func main() {
 	var lsm = CreateLsm(4, 4)
 	lsm.IsCompactionNeeded(2)
-	ReadFiles("structures")
-	fmt.Println("sdsfdsf")
+	ReadFiles("./structures/", 1)
 }
